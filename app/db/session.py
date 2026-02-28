d8624eb16af7bc3b0e8486859ecfffed16129984
FRAIZS <eskeninkarim@gmail.com>
2026-03-01 00:08:38 +0600

    create database config and user model

---
+++ app/db/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Базовый класс для всех моделей 
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
+from app.db.session import engine, Base
+from app.models import user
+
+#Создание таблиц
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="API клон яндекс формы"
 )
 
 @app.get("/")
 def root():
-    return {"message" : "hello world"}
+    return {"message" : "hello world, table created"}
